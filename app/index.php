50f0230a70b8f10d78d062b74f1771d87b447078
valente2905 <valenteqs11@gmail.com>
2025-03-05 08:50:57 -0600

    Update index.php

--- app/index.php
+++ app/index.php
@@ -2,7 +2,7 @@
 <html lang="es">
 <head>
     <!-- esto es un comentario de valente para ver si le se al git jaja -->
-
+    <!-- esto es un comentario de valente 2  -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema Escolar - TecJalisco</title>
